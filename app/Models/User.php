984a4d2c6b784511135c0920bbb96cc1353f5eaa
Personal Profile <davina.leong.sy@gmail.com>
2020-09-10 17:56:34 +0800

    Tested for whether the user has a role

--- app/Models/User.php
+++ app/Models/User.php
@@ -62,4 +62,14 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    /**
+     * Get the associated role of the user.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function role()
+    {
+        return $this->belongsTo('App\Models\Role');
+    }
 }
