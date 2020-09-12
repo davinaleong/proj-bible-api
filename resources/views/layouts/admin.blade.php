87933bb4918e2cadaaac166ad4eeebe9112a0ae7
Personal Profile <davina.leong.sy@gmail.com>
2020-09-12 12:00:10 +0800

    WIP logout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -35,10 +35,12 @@
     <!-- End of Sidebar -->
 
     <!-- Content Wrapper -->
-    <div id="content-wrapper" class="d-flex flex-column pt-3">
+    <div id="content-wrapper" class="d-flex flex-column">
 
         <!-- Main Content -->
         <div id="content">
+
+            @include ('partials.topbar')
 
             <!-- Begin Page Content -->
             <div class="container-fluid">
