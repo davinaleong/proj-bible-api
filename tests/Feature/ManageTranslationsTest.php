8dff35202e093b5bea692a84b5f1aa9363c4ce62
Personal Profile <davina.leong.sy@gmail.com>
2020-09-24 17:38:06 +0800

    Test endpoint accessibility

--- tests/Feature/ManageTranslationsTest.php
+++ tests/Feature/ManageTranslationsTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Models\Translation;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -13,32 +15,58 @@ class ManageTranslationsTest extends TestCase
     /** @test */
     public function guest_cannot_access_endpoints()
     {
-        $this->get(route('translation.index'))
+        $this->get(route('translations.index'))
             ->assertStatus(302)
             ->assertRedirect('login');
 
-        $this->get(route('translation.create'))
+        $this->get(route('translations.create'))
             ->assertStatus(302)
             ->assertRedirect('login');
 
-        $this->post(route('translation.store'))
+        $this->post(route('translations.store'))
             ->assertStatus(302)
             ->assertRedirect('login');
 
-        $this->get(route('translation.show', ['translation' => 1]))
+        $this->get(route('translations.show', ['translation' => 1]))
             ->assertStatus(302)
             ->assertRedirect('login');
 
-        $this->get(route('translation.edit', ['translation' => 1]))
+        $this->get(route('translations.edit', ['translation' => 1]))
             ->assertStatus(302)
             ->assertRedirect('login');
 
-        $this->patch(route('translation.update', ['translation' => 1]))
+        $this->patch(route('translations.update', ['translation' => 1]))
             ->assertStatus(302)
             ->assertRedirect('login');
 
-        $this->delete(route('translation.destroy', ['translation' => 1]))
+        $this->delete(route('translations.destroy', ['translation' => 1]))
             ->assertStatus(302)
             ->assertRedirect('login');
+    }
+
+    /** @test */
+    public function user_can_access_endpoints()
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user)
+            ->get(route('translations.index'))
+            ->assertOk();
+
+        $this->actingAs($user)
+            ->get(route('translations.create'))
+            ->assertOk();
+
+        $translation = Translation::factory()->create([
+            'created_by' => $user->id,
+            'updated_by' => $user->id
+        ]);
+        $this->actingAs($user)
+            ->get(route('translations.show', ['translation' => $translation]))
+            ->assertOk();
+
+        $this->actingAs($user)
+            ->get(route('translations.edit', ['translation' => $translation]))
+            ->assertOk();
     }
 }
