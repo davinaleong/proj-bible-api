5464f492680699e713a1891ce6c7a030d0b7c101
Personal Profile <davina.leong.sy@gmail.com>
2020-09-30 17:04:15 +0800

    Surround page content in a card

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,11 +1,16 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h1 class="h3 mb-2 text-gray-800">{{ $user->name }}</h1>
+    <div class="card shadow">
+        <div class="card-header">
+            <h1 class="card-title h3 mb-2 text-gray-800">{{ $user->name }}</h1>
+        </div>
+        <div class="card-body">
+            <p>Email: <strong>{{ $user->email }}</strong></p>
 
-    <p>Email: <strong>{{ $user->email }}</strong></p>
-
-    <a href="{{ route('users.edit', ['user' => $user]) }}" class="btn btn-primary">
-        Edit <i class="fas fa-pen"></i>
-    </a>
+            <a href="{{ route('users.edit', ['user' => $user]) }}" class="btn btn-primary">
+                Edit <i class="fas fa-pen"></i>
+            </a>
+        </div>
+    </div>
 @endsection
