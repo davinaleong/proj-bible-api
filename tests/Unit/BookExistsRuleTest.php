6b7688a2a0b40b7b008742ea43d9861f2658734e
Personal Profile <davina.leong.sy@gmail.com>
2020-10-02 16:03:19 +0800

    Added BookExists rule

---
+++ tests/Unit/BookExistsRuleTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Book;
+use App\Models\Translation;
+use App\Rules\BookExists;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class BookExistsRuleTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function rule_passes_when_no_book_of_translation_exists()
+    {
+        $translation = Translation::factory()->create();
+        $rule = new BookExists($translation);
+        $this->assertTrue($rule->passes('name', 'Book'));
+    }
+
+    /** @test */
+    public function rule_passes_when_book_of_different_translation_exists()
+    {
+        $translations = Translation::factory()->count(2)->create();
+        $book = Book::factory()->create([
+            'translation_id' => $translations[1],
+            'name' => 'Book'
+        ]);
+        $rule = new BookExists($translations[0]);
+        $this->assertTrue($rule->passes('name', $book->name));
+    }
+
+    /** @test */
+    public function rule_fails_when_book_of_same_translation_exists()
+    {
+        $translation = Translation::factory()->create();
+        $book = Book::factory()->create([
+            'translation_id' => $translation,
+            'name' => 'Book'
+        ]);
+        $rule = new BookExists($translation);
+        $this->assertFalse($rule->passes('name', $book->name));
+    }
+
+    /** @test */
+    public function rule_passes_when_book_is_the_same_id()
+    {
+        $translation = Translation::factory()->create();
+        $book = Book::factory()->create([
+            'translation_id' => $translation,
+            'name' => 'Book'
+        ]);
+        $rule = new BookExists($translation, $book);
+        $this->assertTrue($rule->passes('name', $book->name));
+    }
+}
