59060b267210d4961c29ca9895b7dea30d0ffe8f
Personal Profile <davina.leong.sy@gmail.com>
2020-11-29 15:56:39 +0800

    Added create chapter page

---
+++ resources/views/chapters/create.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>{{ $book->name }}</h1>
+
+    <div class="card shadow">
+        <div class="card-header">
+            <h1 class="card-title h5">Create Chapter</h1>
+        </div>
+        <div class="card-body">
+            <form method="post" action="{{ route('chapters.store', ['translation' => $translation, 'book' => $book]) }}">
+                @csrf
+                <div class="form-group">
+                    <label>Book</label>
+                    <p class="form-control-plaintext">{{ $book->name }}</p>
+                </div>
+                <div class="form-group">
+                    <label for="number">Number <span class="text-danger">*</span></label>
+                    <input type="text" name="number" class="form-control" value="{{ old('number') }}" required>
+                    <p class="form-text text-muted">
+                        <small>Determines the order of the chapter.</small>
+                    </p>
+                </div>
+                <div class="form-group">
+                    <label for="verse_limit">Verse Limit <span class="text-danger">*</span></label>
+                    <input type="text" name="verse_limit" class="form-control" value="{{ old('verse_limit') }}" required>
+                    <p class="form-text text-muted">
+                        <small>A check for the number of verses one can create for this chapter.</small>
+                    </p>
+                </div>
+
+                <p class="text-danger">* required</p>
+                <button type="submit" class="btn btn-primary">
+                    Submit <i class="fas fa-check"></i>
+                </button>
+                <a href="{{ route('books.show', ['translation' => $translation, 'book' => $book]) }}" class="btn btn-outline-secondary">
+                    Cancel <i class="fas fa-ban"></i>
+                </a>
+            </form>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/chapters/show.blade.php
@@ -0,0 +1,102 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>{{ $translation->name }}</h1>
+
+    <div class="card shadow mb-3">
+        <div class="card-header">
+            <h2 class="h5 card-title">{{ $book->name }}</h2>
+        </div>
+        <div class="card-body">
+            <p>Abbr: {{ $book->abbr }}</p>
+            <p>Number: {{ $book->number }}</p>
+            <p>Chapter Limit: {{ $book->chapter_limit }}</p>
+            <p>Creator: {{ $book->getCreatorName() }}</p>
+            <p>Updater: {{ $book->getUpdaterName() }}</p>
+
+            <a href="{{ route('books.edit', ['translation' => $translation, 'book' => $book]) }}" class="btn btn-primary">Edit <i class="fas fa-pen"></i></a>
+            <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteModal">
+                Delete <i class="fas fa-trash-alt"></i>
+            </button>
+        </div>
+    </div>
+
+    <div class="card shadow">
+        <div class="card-header">
+            <h2 class="h5 card-title">
+                Chapters ({{ $book->chapters->count() }})
+                @if ($book->chapters->count() < $book->chapter_limit)
+                    <a href="{{ route('chapters.create', ['translation' => $translation, 'book' => $book]) }}" class="btn btn-primary">
+                        Create <i class="fas fa-plus"></i>
+                    </a>
+                @endif
+            </h2>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table id="table" class="table table-bordered table-hover" width="100%" cellspacing="0">
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Number</th>
+                        <th>Verse Limit</th>
+                        <th>Created At</th>
+                        <th>Updated At</th>
+                    </tr>
+                    </thead>
+                    <tfoot>
+                    <tr>
+                        <th>ID</th>
+                        <th>Number</th>
+                        <th>Verse Limit</th>
+                        <th>Created At</th>
+                        <th>Updated At</th>
+                    </tr>
+                    </tfoot>
+                    <tbody>
+                    @foreach($book->chapters as $chapter)
+                        <tr class="clickable" onclick="goto('{{ route('chapters.show', ['translation' => $translation, 'book' => $book, 'chapter' => $chapter]) }}')">
+                            <td>{{ $chapter->id }}</td>
+                            <td>{{ $chapter->number }}</td>
+                            <td>{{ $chapter->verse_limit }}</td>
+                            <td>{{ $chapter->getCreatorName() }}</td>
+                            <td>{{ $chapter->getUpdaterName() }}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <!-- Delete Modal-->
+    <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <form method="post" action="{{ route('books.destroy', ['translation' => $translation, 'book' => $book]) }}">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Delete Book</h5>
+                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        @csrf
+                        @method('DELETE')
+                        Are you sure?
+                        <br>This will delete <strong>ALL</strong> chapters and verses associated with this book.
+                        <br><span class="text-danger">This action cannot be undone.</span>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn btn-outline-secondary" type="button" data-dismiss="modal">
+                            Cancel <i class="fas fa-ban"></i>
+                        </button>
+                        <button type="submit" class="btn btn-danger">
+                            Delete <i class="fas fa-trash-alt"></i>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
