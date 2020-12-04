65ab6a0ec14af059412733078383c20496455418
Personal Profile <davina.leong.sy@gmail.com>
2020-12-05 07:18:15 +0800

    Get translation timestamps

--- app/Models/Translation.php
+++ app/Models/Translation.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Events\TranslationCreated;
 use App\Events\TranslationDeleted;
 use App\Events\TranslationUpdated;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,18 +13,24 @@ class Translation extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'name',
-        'abbr',
-        'copyright_id',
-        'created_by',
-        'updated_by'
+    protected $guarded = [
+        'created_at',
+        'updated_at'
+    ];
+
+    protected $hidden = [
+        'deleted_at'
     ];
 
     protected $dispatchesEvents = [
         'created' => TranslationCreated::class,
         'updated' => TranslationUpdated::class,
         'deleted' => TranslationDeleted::class
+    ];
+
+    private $dateFormats = [
+        'db' => 'Y-m-d H:i:s',
+        'show' => 'H:i:s d-m-Y'
     ];
 
     public function copyright()
@@ -59,5 +66,25 @@ class Translation extends Model
     public function getUpdaterName()
     {
         return $this->updater ? $this->updater->name : '';
+    }
+
+    public function getCreatedAt()
+    {
+        if ($this->created_at) {
+            $dt = Carbon::createFromFormat($this->dateFormats['db'], $this->created_at);
+            return $dt->format($this->dateFormats['show']);
+        } else {
+            return '';
+        }
+    }
+
+    public function getUpdatedAt()
+    {
+        if ($this->created_at) {
+            $dt = Carbon::createFromFormat($this->dateFormats['db'], $this->updated_at);
+            return $dt->format($this->dateFormats['show']);
+        } else {
+            return '';
+        }
     }
 }
