8b5c31e1f6d08a3e77623056348c2a9ecbccb3ae
Personal Profile <davina.leong.sy@gmail.com>
2020-12-18 17:57:45 +0800

    Added unit tests for Verse

---
+++ tests/Unit/VerseTest.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Chapter;
+use App\Models\User;
+use App\Models\Verse;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class VerseTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function has_a_creator()
+    {
+        $verse = Verse::factory()->create();
+
+        $this->assertInstanceOf(User::class, $verse->creator);
+    }
+
+    /** @test */
+    public function has_an_updater()
+    {
+        $verse = Verse::factory()->create();
+
+        $this->assertInstanceOf(User::class, $verse->updater);
+    }
+
+    /** @test */
+    public function has_a_chapter()
+    {
+        $verse = Verse::factory()->create();
+
+        $this->assertInstanceOf(Chapter::class, $verse->chapter);
+    }
+
+    /** @test */
+    public function get_creator_name()
+    {
+        $user = User::factory()->create();
+        $chapter = Chapter::factory()->create([
+            'created_by' => $user->id
+        ]);
+
+        $this->assertEquals($user->name, $chapter->getCreatorName());
+    }
+
+    /** @test */
+    public function get_updater_name()
+    {
+        $user = User::factory()->create();
+        $chapter = Chapter::factory()->create([
+            'updated_by' => $user->id
+        ]);
+
+        $this->assertEquals($user->name, $chapter->getUpdaterName());
+    }
+
+    /** @test */
+    public function get_creator_name_returns_null_if_no_creator()
+    {
+        $chapter = Chapter::factory()->create([
+            'created_by' => null
+        ]);
+
+        $this->assertEquals('', $chapter->getCreatorName());
+    }
+
+    /** @test */
+    public function get_updater_name_returns_null_if_no_updater()
+    {
+        $chapter = Chapter::factory()->create([
+            'updated_by' => null
+        ]);
+
+        $this->assertEquals('', $chapter->getUpdaterName());
+    }
+
+    /** @test */
+    public function get_chapter_number()
+    {
+        $chapter = Chapter::factory()->create();
+        $verse = Verse::factory()->create([
+            'chapter_id' => $chapter->id
+        ]);
+
+        $this->assertEquals($chapter->number, $verse->getChapterNumber());
+    }
+
+    /** @test */
+    public function get_verse_returns_verse_of_chapter_and_number()
+    {
+        $chapter = Chapter::factory()->create();
+        $verse = Verse::factory()->create([
+            'chapter_id' => $chapter->id,
+            'number' => 1
+        ]);
+
+        $this->assertEquals($verse->id, Verse::getVerse($chapter, $verse->number)->id);
+    }
+}
