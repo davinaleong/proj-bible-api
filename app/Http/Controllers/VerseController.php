ff0c28b4c4a743272aa73077ffc3a7564687b57e
Personal Profile <davina.leong.sy@gmail.com>
2020-12-20 18:34:25 +0800

    Added create verse page

--- app/Http/Controllers/VerseController.php
+++ app/Http/Controllers/VerseController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
+use App\Models\Breadcrumb;
 use App\Models\Chapter;
 use App\Models\Translation;
 use App\Models\Verse;
@@ -13,7 +14,29 @@ class VerseController extends Controller
 {
     public function create(Translation $translation, Book $book, Chapter $chapter)
     {
-        //
+        return view('verses.create', [
+            'breadcrumb' => Breadcrumb::items([
+                [
+                    'label' => 'Translations',
+                    'href' => route('translations.index')
+                ], [
+                    'label' => $translation->abbr,
+                    'href' => route('translations.show', ['translation' => $translation])
+                ], [
+                    'label' => $book->abbr,
+                    'href' => route('books.show', ['translation' => $translation, 'book' => $book])
+                ], [
+                    'label' => $chapter->number,
+                    'href' => route('chapters.show', ['translation' => $translation, 'book' => $book, 'chapter' => $chapter])
+                ], [
+                    'label' => 'Create Verse',
+                    'active' => true
+                ]
+            ]),
+            'translation' => $translation,
+            'book' => $book,
+            'chapter' => $chapter
+        ]);
     }
 
     public function store(Translation $translation, Book $book, Chapter $chapter)
---
+++ resources/views/verses/create.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>{{ $translation->name }}</h1>
+
+    <div class="card shadow">
+        <div class="card-header">
+            <h1 class="card-title h5">Book: {{ $book->name }}, Chapter: {{ $chapter->number }} Create Verse</h1>
+        </div>
+        <div class="card-body">
+            <form method="post" action="{{ route('verses.store', ['translation' => $translation, 'book' => $book, 'chapter' => $chapter]) }}">
+                @csrf
+                <div class="form-group">
+                    <label>Chapter</label>
+                    <p class="form-control-plaintext">{{ $chapter->number }}</p>
+                </div>
+                <div class="form-group">
+                    <label for="number">Number <span class="text-danger">*</span></label>
+                    <input type="text" name="number" class="form-control" value="{{ old('number') }}" required>
+                    <p class="form-text text-muted">
+                        <small>Determines the order of the verse.</small>
+                    </p>
+                </div>
+                <div class="form-group">
+                    <label for="passage">Passage <span class="text-danger">*</span></label>
+                    <textarea name="passage" class="form-control">{{ old('passage') }}</textarea>
+                </div>
+
+                <p class="text-danger">* required</p>
+                <button type="submit" class="btn btn-primary">
+                    Submit <i class="fas fa-check"></i>
+                </button>
+                <a href="{{ route('chapters.show', ['translation' => $translation, 'book' => $book, 'chapter' => $chapter]) }}" class="btn btn-outline-secondary">
+                    Cancel <i class="fas fa-ban"></i>
+                </a>
+            </form>
+        </div>
+    </div>
+@endsection
