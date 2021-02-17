fb52f596d60a60bc986666e2cb2cb73ddbf29ee0
Personal Profile <davina.leong.sy@gmail.com>
2021-02-17 18:00:43 +0800

    Edited copyright

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; {{ config('app.name') }} 2020</span>
+            <span>{{ config('app.name') }} &copy; Davina Leong, 2020-{{ now()->format('Y') }}</span>
         </div>
     </div>
 </footer>
