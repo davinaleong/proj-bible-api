45482a65fdc221ab816eacc0bf2c7b53fcf636b3
Personal Profile <davina.leong.sy@gmail.com>
2021-02-20 16:09:03 +0800

    Added verse limit check rule

---
+++ app/Rules/VerseLimitCheck.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class VerseLimitCheck implements Rule
+{
+    private $chapter;
+
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct($chapter)
+    {
+        $this->chapter = $chapter;
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        if (!is_numeric($value)) return true;
+
+        return ($this->chapter->verse_limit >= $value);
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The :attribute must be less than or equal :value.';
+    }
+}
---
+++ tests/Unit/VerseLimitCheckTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Chapter;
+use App\Rules\VerseLimitCheck;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class VerseLimitCheckTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function rule_passes_if_value_is_a_string()
+    {
+        $chapter = Chapter::factory()->create();
+        $rule = new VerseLimitCheck($chapter);
+
+        $this->assertTrue($rule->passes('number', '1-2'));
+    }
+
+    /** @test */
+    public function rule_passes_if_value_is_less_or_equal_to_verse_limit()
+    {
+        $chapter = Chapter::factory()->create([
+            'verse_limit' => 2
+        ]);
+        $rule = new VerseLimitCheck($chapter);
+
+        $this->assertTrue($rule->passes('number', '1'));
+    }
+
+    /** @test */
+    public function rule_fails_if_value_is_greater_than_verse_limit()
+    {
+        $chapter = Chapter::factory()->create([
+            'verse_limit' => 1
+        ]);
+        $rule = new VerseLimitCheck($chapter);
+
+        $this->assertFalse($rule->passes('number', '2'));
+    }
+}
