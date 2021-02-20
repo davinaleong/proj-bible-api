25cb092db8e5ed18c3dded4b29ba5f702ea78477
Personal Profile <davina.leong.sy@gmail.com>
2021-02-20 17:09:34 +0800

    Fixing unique translation abbr error

--- tests/Unit/BookNumberExistsTest.php
+++ tests/Unit/BookNumberExistsTest.php
@@ -31,9 +31,16 @@ class BookNumberExistsTest extends TestCase
     /** @test */
     public function rule_passes_when_book_of_different_translation_exists()
     {
-        $translations = Translation::factory()->count(2)->create();
+        $translations = [
+            Translation::factory()->create([
+                'abbr' => 'T'
+            ]),
+            Translation::factory()->create([
+                'abbr' => 'T2'
+            ])
+        ];
         $book = Book::factory()->create([
-            'translation_id' => $translations[1],
+            'translation_id' => $translations[1]->id,
             'number' => 1
         ]);
         $rule = new BookNumberExists($translations[0]);
@@ -43,24 +50,20 @@ class BookNumberExistsTest extends TestCase
     /** @test */
     public function rule_fails_when_book_of_same_translation_exists()
     {
-        $translation = Translation::factory()->create();
         $book = Book::factory()->create([
-            'translation_id' => $translation,
             'number' => 1
         ]);
-        $rule = new BookNumberExists($translation);
+        $rule = new BookNumberExists($book->translation);
         $this->assertFalse($rule->passes('number', $book->number));
     }
 
     /** @test */
     public function rule_passes_when_book_is_the_same_id()
     {
-        $translation = Translation::factory()->create();
         $book = Book::factory()->create([
-            'translation_id' => $translation,
             'number' => 1
         ]);
-        $rule = new BookNumberExists($translation, $book);
+        $rule = new BookNumberExists($book->translation, $book);
         $this->assertTrue($rule->passes('number', $book->number));
     }
 }
